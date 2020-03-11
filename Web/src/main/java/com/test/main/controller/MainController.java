99b10ba7c8c1bd826d6f7931a845d21e6749ec82
Jaewook <Jaewook@DESKTOP-NHP7CJ2>
2020-03-11 20:42:47 +0900

    Add MainController
    Add MainController.java for @Controller

--- Web/src/main/java/com/test/main/controller/MainController.java
+++ Web/src/main/java/com/test/main/controller/MainController.java
@@ -1,13 +1,28 @@
 package com.test.main.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.test.main.dto.Member;
+import com.test.main.service.MainService;
+
+@Controller
 public class MainController {
 
-	@GetMapping("/index")
-	public String index(){
+	@Autowired
+	private MainService mainService;
+	
+	@GetMapping("/")
+	public String index(Model model){
 		
-		System.out.println("index() MainController");
+		List<Member> memberList = mainService.getMemberList();
+		//System.out.println(memberList + " <-- memberList index() MainController.java");
+		
+		model.addAttribute("memberList", memberList);
 		
 		return "index";
 	}
