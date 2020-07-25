930d286ad099848cba992fef6b6964f08254401c
Jaewook <Jaewook@DESKTOP-NHP7CJ2>
2020-07-25 14:55:21 +0900

    Create : MemberPlayerDTO
    Create MemberPlayerDTO for MEMBER_PLAYER table

---
+++ Web/src/main/java/com/test/main/dto/MemberPlayerDTO.java
@@ -0,0 +1,105 @@
+package com.test.main.dto;
+
+public class MemberPlayerDTO {
+
+	// 변수 선언
+	private String sPlayerNO;
+	private String sMemberNO;
+	private String sPlayerDivision;
+	private String sPlayerName;
+	private String sPlayerYear;
+	private String sPlayerType;
+	private String sPlayerBattingOrder;
+	private String sPlayerAge;
+	private String sPlayerLevel;
+	private String sPlayerPositionMain;
+	private String sPlayerPositionSub;
+	private String sPlayerClass;
+	private String sPlayerTeam;
+	private String sPlayerOverall;
+	
+	public String getsPlayerNO() {
+		return sPlayerNO;
+	}
+	public void setsPlayerNO(String sPlayerNO) {
+		this.sPlayerNO = sPlayerNO;
+	}
+	public String getsMemberNO() {
+		return sMemberNO;
+	}
+	public void setsMemberNO(String sMemberNO) {
+		this.sMemberNO = sMemberNO;
+	}
+	public String getsPlayerDivision() {
+		return sPlayerDivision;
+	}
+	public void setsPlayerDivision(String sPlayerDivision) {
+		this.sPlayerDivision = sPlayerDivision;
+	}
+	public String getsPlayerName() {
+		return sPlayerName;
+	}
+	public void setsPlayerName(String sPlayerName) {
+		this.sPlayerName = sPlayerName;
+	}
+	public String getsPlayerYear() {
+		return sPlayerYear;
+	}
+	public void setsPlayerYear(String sPlayerYear) {
+		this.sPlayerYear = sPlayerYear;
+	}
+	public String getsPlayerType() {
+		return sPlayerType;
+	}
+	public void setsPlayerType(String sPlayerType) {
+		this.sPlayerType = sPlayerType;
+	}
+	public String getsPlayerBattingOrder() {
+		return sPlayerBattingOrder;
+	}
+	public void setsPlayerBattingOrder(String sPlayerBattingOrder) {
+		this.sPlayerBattingOrder = sPlayerBattingOrder;
+	}
+	public String getsPlayerAge() {
+		return sPlayerAge;
+	}
+	public void setsPlayerAge(String sPlayerAge) {
+		this.sPlayerAge = sPlayerAge;
+	}
+	public String getsPlayerLevel() {
+		return sPlayerLevel;
+	}
+	public void setsPlayerLevel(String sPlayerLevel) {
+		this.sPlayerLevel = sPlayerLevel;
+	}
+	public String getsPlayerPositionMain() {
+		return sPlayerPositionMain;
+	}
+	public void setsPlayerPositionMain(String sPlayerPositionMain) {
+		this.sPlayerPositionMain = sPlayerPositionMain;
+	}
+	public String getsPlayerPositionSub() {
+		return sPlayerPositionSub;
+	}
+	public void setsPlayerPositionSub(String sPlayerPositionSub) {
+		this.sPlayerPositionSub = sPlayerPositionSub;
+	}
+	public String getsPlayerClass() {
+		return sPlayerClass;
+	}
+	public void setsPlayerClass(String sPlayerClass) {
+		this.sPlayerClass = sPlayerClass;
+	}
+	public String getsPlayerTeam() {
+		return sPlayerTeam;
+	}
+	public void setsPlayerTeam(String sPlayerTeam) {
+		this.sPlayerTeam = sPlayerTeam;
+	}
+	public String getsPlayerOverall() {
+		return sPlayerOverall;
+	}
+	public void setsPlayerOverall(String sPlayerOverall) {
+		this.sPlayerOverall = sPlayerOverall;
+	}
+}
